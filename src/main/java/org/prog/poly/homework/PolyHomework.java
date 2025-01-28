8b5054baf2d78ce6dca5ee58681975985438bdf6
Oleg <osipovicho@gmail.com>
2025-01-28 11:54:12 +0200

    hw2

--- src/main/java/org/prog/poly/homework/PolyHomework.java
+++ src/main/java/org/prog/poly/homework/PolyHomework.java
@@ -4,7 +4,7 @@ package org.prog.poly.homework;
 //TODO: Add interface method for call
 //TODO: Add interface method for unlock
 //TODO: Add class for Androind and iOS phones that implement IPhone
-
+   //asdf
 public class PolyHomework {
 
     public static void main(String[] args) {
