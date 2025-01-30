7fbec2e773598e18fb89f6f8d786e4b9d314a124
Oleg <osipovicho@gmail.com>
2025-01-30 16:36:46 +0200

    hw3

--- src/main/java/org/prog/exceptions/ExceptionsHomework.java
+++ src/main/java/org/prog/exceptions/ExceptionsHomework.java
@@ -1,9 +1,42 @@
 package org.prog.exceptions;
 
 
+import java.io.FileNotFoundException;
+
 //TODO: write a method which ca cause NullPointerException
 // TODO: write try-catc-finally to catch NPE
 // TODO: on exception -> print "EXCEPTION CAUGHT!"
 // TODO: always print "DONE TRY-CATCH-FINALLY"  asd
 public class ExceptionsHomework {
+    public static void main(String[] args) {
+
+
+        try {
+            Integer num = 10;
+            Integer num2 = null;
+
+            System.out.println(num2.equals(num));
+
+        } catch (NullPointerException npe) {
+            System.out.println("EXCEPTION CAUGHT!");
+
+        } finally {
+            System.out.println("DONE TRY-CATCH-FINALLY");
+        }
+    }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
