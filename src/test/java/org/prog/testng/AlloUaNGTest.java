c537b25c5dbe605db5c03621cf5a92419e2253e7
Oleg <osipovicho@gmail.com>
2025-02-25 08:33:58 +0200

    hw7

--- src/test/java/org/prog/testng/AlloUaNGTest.java
+++ src/test/java/org/prog/testng/AlloUaNGTest.java
@@ -2,7 +2,13 @@ package org.prog.testng;
 
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 //TODO: create separate test-ng.xml file with this class only
 //TODO: move your selenium test to this class
 //TODO: Add assertion that goods name is not null
@@ -54,4 +60,39 @@ public class AlloUaNGTest {
     public void ngTest5() {
         System.out.println("Hello Allo UA Test! 4");
     }
+
+    @Test
+    public void ngTest6() {
+
+
+        WebDriver driver = null;
+        try {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://allo.ua/");
+
+            //Thread.sleep(2000);
+            System.out.println(driver.getTitle());
+            WebElement searchInput = driver.findElement(By.id("search-form__input"));
+            searchInput.sendKeys(String.valueOf("Xiaomi"));
+            searchInput.sendKeys(Keys.ENTER);
+            //Thread.sleep(2000);
+            System.out.println(driver.getTitle());
+
+
+            WebElement questionElement = driver.findElement(By.xpath("//a[contains(@title,'Xiaomi')]"));
+            questionElement.getText();
+            System.out.println(questionElement.getText());
+
+
+
+
+        }
+        finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
 }
