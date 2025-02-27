0348266f0072e0b1b281959073e833d86fdb8b0d
Oleg <osipovicho1988@gmail.com>
2025-02-27 11:53:38 +0300

    HW7

--- src/test/java/org/prog/testng/AlloUaNGTest.java
+++ src/test/java/org/prog/testng/AlloUaNGTest.java
@@ -15,7 +15,47 @@ import java.util.List;
 //TODO: Add assertion that goods name has > 0 length
 
 public class AlloUaNGTest {
+    public static void main(String[] args) throws InterruptedException {
 
+        WebDriver driver = null;
+        try {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://allo.ua/");
+
+            Thread.sleep(2000);
+            System.out.println(driver.getTitle());
+            WebElement searchInput = driver.findElement(By.id("search-form__input"));
+            searchInput.sendKeys(String.valueOf("Xiaomi"));
+            searchInput.sendKeys(Keys.ENTER);
+            Thread.sleep(2000);
+            System.out.println(driver.getTitle());
+
+
+            WebElement questionElement = driver.findElement(By.xpath("//a[contains(@title,'Xiaomi')]"));
+            questionElement.getText();
+            System.out.println(questionElement.getText());
+
+            if (questionElement.getText().length() >0) {
+                System.out.println("goods name/string length = " + questionElement.getText().length());
+            };
+            if (questionElement.getText() == null) {
+                System.out.println("object is Null");
+            } else {
+                System.out.println("object Not Null");
+            };
+
+
+
+
+
+        }
+        finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
 //    @BeforeClass
 //    public void beforeAllo() {
 //        System.out.println(">>>>>>>>>>ALLO.START");
@@ -61,38 +101,5 @@ public class AlloUaNGTest {
         System.out.println("Hello Allo UA Test! 4");
     }
 
-    @Test
-    public void ngTest6() {
-
-
-        WebDriver driver = null;
-        try {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.get("https://allo.ua/");
-
-            //Thread.sleep(2000);
-            System.out.println(driver.getTitle());
-            WebElement searchInput = driver.findElement(By.id("search-form__input"));
-            searchInput.sendKeys(String.valueOf("Xiaomi"));
-            searchInput.sendKeys(Keys.ENTER);
-            //Thread.sleep(2000);
-            System.out.println(driver.getTitle());
-
-
-            WebElement questionElement = driver.findElement(By.xpath("//a[contains(@title,'Xiaomi')]"));
-            questionElement.getText();
-            System.out.println(questionElement.getText());
-
-
-
-
-        }
-        finally {
-            if (driver != null) {
-                driver.quit();
-            }
-        }
-    }
 
 }
